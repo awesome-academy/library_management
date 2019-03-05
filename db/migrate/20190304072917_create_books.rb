637daf8137b64595b4a1454fd3aa94190fae9e1b
phambadat <phambadat.97@gmail.com>
2019-03-05 11:34:12 +0700

    detail of book

--- db/migrate/20190304072917_create_books.rb
+++ db/migrate/20190304072917_create_books.rb
@@ -3,7 +3,6 @@ class CreateBooks < ActiveRecord::Migration[5.2]
     create_table :books do |t|
       t.integer :category_id
       t.integer :publisher_id
-      t.integer :author_id
       t.string :name
       t.string :content
       t.integer :quantity_of_pages
