ecaf6dd74ced5681ad085cf1d4b8fc3344672084
phambadat <phambadat.97@gmail.com>
2019-03-15 09:25:12 +0700

    review book

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,47 @@
+class ReviewsController < ApplicationController
+  before_action :load_review, only: :destroy
+
+  def show
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def create
+    @review = Review.new review_params
+    if @review.save
+      @book = @review.book
+      flash[:success] = t "review.successful"
+    else
+      flash[:danger] = t "review.fails"
+    end
+    respond_to do |format|
+      format.js {}
+    end
+  end
+
+  def destroy
+    if @review.destroy
+      @book = @review.book
+      flash[:success] = t "review.delete"
+    else
+      flash[:danger] = t "review.fails"
+    end
+    respond_to do |format|
+      format.js {}
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit :content, :user_id, :book_id
+  end
+
+  def load_review
+    @review = Review.find_by id: params[:id]
+    return if @review
+    flash[:danger] = t "messenger.no_data"
+    redirect_to book_path
+  end
+end
